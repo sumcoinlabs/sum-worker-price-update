fde7c0614510a73bedde3ad1425cfcc6415261e0
Willy <willy@coinerella.com>
2021-08-13 15:44:36 +0200

    prettier

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 async function gatherResponse(response) {
   const { headers } = response
-  const contentType = headers.get("content-type") || ""
-  if (contentType.includes("application/json")) {
+  const contentType = headers.get('content-type') || ''
+  if (contentType.includes('application/json')) {
     return await response.json()
-  }
-  else {
+  } else {
     return response.text()
   }
 }
@@ -12,46 +11,48 @@ async function gatherResponse(response) {
 async function getFromApi(url) {
   const init = {
     headers: {
-      "content-type": "application/json",
+      'content-type': 'application/json',
     },
   }
   const response = await fetch(url, init)
   const results = await gatherResponse(response)
-  return results; 
+  return results
 }
 
-addEventListener("fetch", event => {
+addEventListener('fetch', (event) => {
   event.waitUntil(handleSchedule())
   return event.respondWith(new Response())
 })
 
-addEventListener('scheduled', event => {
-  event.waitUntil(
-    handleSchedule(event.scheduledTime)
-  )
+addEventListener('scheduled', (event) => {
+  event.waitUntil(handleSchedule(event.scheduledTime))
 })
 
 async function handleSchedule() {
-  //get coinpaprika ppc/usd price 
-  const paprikaResponse = await getFromApi("https://api.coinpaprika.com/v1/tickers/ppc-peercoin");
-  const openExchangeResponse = await getFromApi(`https://openexchangerates.org/api/latest.json?app_id=${CURRCONV_KEY}`);
-  const ppcUsdPrice = paprikaResponse["quotes"]["USD"]["price"];
+  //get coinpaprika ppc/usd price
+  const paprikaResponse = await getFromApi(
+    'https://api.coinpaprika.com/v1/tickers/ppc-peercoin',
+  )
+  const openExchangeResponse = await getFromApi(
+    `https://openexchangerates.org/api/latest.json?app_id=${CURRCONV_KEY}`,
+  )
+  const ppcUsdPrice = paprikaResponse['quotes']['USD']['price']
 
   //get currency fiat/usd exchange rates
-  const rates = openExchangeResponse["rates"];
+  const rates = openExchangeResponse['rates']
   const fiatPricesUSD = {
-    ARS: rates["ARS"],
-    BRL: rates["BRL"],
-    CNY: rates["CNY"],
-    EUR: rates["EUR"],
-    GBP: rates["GBP"],
-    HRK: rates["HRK"],
-    INR: rates["INR"],
-    RON: rates["RON"],
-    RUB: rates["RUB"],
-  };
+    ARS: rates['ARS'],
+    BRL: rates['BRL'],
+    CNY: rates['CNY'],
+    EUR: rates['EUR'],
+    GBP: rates['GBP'],
+    HRK: rates['HRK'],
+    INR: rates['INR'],
+    RON: rates['RON'],
+    RUB: rates['RUB'],
+  }
 
   //write to KV
-  await peercoin_kv.put("PPC_USD", ppcUsdPrice);
-  await peercoin_kv.put("FIAT_USD", JSON.stringify(fiatPricesUSD));
+  await peercoin_kv.put('PPC_USD', ppcUsdPrice)
+  await peercoin_kv.put('FIAT_USD', JSON.stringify(fiatPricesUSD))
 }
