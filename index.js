4af9ea40391afa5ef5144d702d15d940b891df8f
Willy <willy@coinerella.com>
2021-12-01 10:31:39 +0100

    add PLN

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ async function handleSchedule() {
     GBP: rates['GBP'],
     HRK: rates['HRK'],
     INR: rates['INR'],
+    PLN: rates['PLN'],
     PPC: parseFloat(ppcUsdPrice.toFixed(6)),
     RON: rates['RON'],
     RUB: rates['RUB'],
